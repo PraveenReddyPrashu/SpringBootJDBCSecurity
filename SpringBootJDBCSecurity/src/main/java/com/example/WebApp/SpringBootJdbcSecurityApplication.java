3e46877b3930874fb9e3d70f3886423ada66b15c
Praveen Reddy <praveenreddy291@gmail.com>
2021-05-12 15:06:51 +0530

    Jdbc with security

---
+++ SpringBootJDBCSecurity/src/main/java/com/example/WebApp/SpringBootJdbcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcSecurityApplication.class, args);
+	}
+
+}
